36b293551da776b8d0804d8ac2ac2b96a56b3921
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-17 19:03:24 -0500

    close

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,43 @@
+# GET +++++++++++++++++++++++++++++++++++++++++++++++++
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/cars' do
+  @cars = Post.where(category_id: 2)
+  erb :cars
+end
+
+get '/jobs' do
+  @jobs = Post.where(category_id: 3)
+  erb :jobs
+end
+
+get '/misc' do
+  @misc = Post.where(category_id: 1)
+  erb :misc
+end
+
+get '/new' do
+  erb :new_post
+end
+
+get '/:post_id' do
+  @post = Post.find(params[:post_id])
+  erb :post
+end
+
+# POST +++++++++++++++++++++++++++++++++++++++++++++++++
+
+post '/:post_id' do
+  @post = Post.create(params[:post])
+  erb :post
+end 
+
+post '/:post_id' do
+  @post = Post.find(params[:post_id])
+  @post.destroy
+  redirect to ('/')
+end 
